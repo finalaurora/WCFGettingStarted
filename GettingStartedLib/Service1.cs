c6a7c264769ca3ece330a19de333ebca9c09303a
ntlam3 <ntlam3@tma.com.vn>
2019-05-22 13:44:03 +0700

    Add WCF service's client app

--- GettingStartedLib/Service1.cs
+++ GettingStartedLib/Service1.cs
@@ -11,7 +11,7 @@ namespace GettingStartedLib
     {
         public double Add(double n1, double n2)
         {
-            double result = n1 - n2;
+            double result = n1 + n2;
             Console.WriteLine("Received Add ({0},{1})", n1, n2);
             Console.WriteLine("Return: {0}", result);
             return result;
